e267115863bfd920f14b7228771969e058dbca60
lbesnard <laurent.besnard@utas.edu.au>
2014-08-14 18:22:08 +1000

    remove some logging code

--- subroutines/bom.py
+++ subroutines/bom.py
@@ -2,24 +2,6 @@
 def station_info_bom(station_name,location):
     import string, re, os, time, smtplib, sys, urllib2, csv, os.path, requests, datetime
     
-    logging.basicConfig(level=logging.INFO)
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.INFO)
-
-    # create a file handler
-    
-    handler = logging.FileHandler('riverHeight.log')
-    handler.setLevel(logging.INFO)
-    
-    # create a logging format
-    
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-    
-    # add the handlers to the logger
-    
-    logger.addHandler(handler)
-    
     # Getting webpage to check according to the river to check
     if location == 'NORTH':
         url = 'http://www.bom.gov.au/cgi-bin/wrap_fwo.pl?IDT60151.html'
@@ -43,7 +25,7 @@ def station_info_bom(station_name,location):
         height = float(mobj.groups()[1].strip('"').replace("^",""))
         status = mobj.groups()[2].replace(" ","")
     else:
-        logger.warning ("WARNING: " + str('station ' + station_name  + ' cannot be found on the BOM webpage'))
+        #logger.warning ("WARNING: " + str('station ' + station_name  + ' cannot be found on the BOM webpage'))
         timeStr = 'N.A.'
         height = float(0)
         status = 'N.A.'
@@ -60,23 +42,7 @@ def station_info_bom(station_name,location):
 def message_BOM(station_name,timeStr,height,status,riverName,stationNickname,paddleTasmaniaLink,BOM_chartlink_short):
     import string, os, time, sys, datetime
     
-    logging.basicConfig(level=logging.INFO)
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.INFO)
-
-    # create a file handler
     
-    handler = logging.FileHandler('riverHeight.log')
-    handler.setLevel(logging.INFO)
-    
-    # create a logging format
-    
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-    
-    # add the handlers to the logger
-    
-    logger.addHandler(handler)
     # create the string message which will be sent by email or twitter
     msg =  riverName+' is ' + str(status)  + ' at ' +stationNickname +':'  +  str(height) + 'm at '+ str(timeStr) + '.See Chart: ' +BOM_chartlink_short + ' Guide: ' + paddleTasmaniaLink  
     if len(msg) > 140:
